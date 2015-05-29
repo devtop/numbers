35e31c31fed90450e5e4db52d9d55eb803c1daad
Tobias Ranft <develop@ranft.biz>
2015-05-29 21:38:19 +0200

    Added prompt application flow.

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-random = require("random");
+var prompt = require('prompt')
+var random = require('random');
 
-console.log("OK, we are starting...");
-console.log(random.createInteger(2));
+var schema = {
+  properties: {
+    numberOfNumbers: {
+      description: 'How many rounds do you want do?'.white,
+      pattern: /^\d+$/,
+      message: 'Number of numbers has to be a number',
+      default: 10
+    },
+    rangeMaximum: {
+      description: 'What should the highest number be?'.white,
+      pattern: /^\d+$/,
+      message: 'The range maximum has to be a number',
+      default: 9999
+    }
+  }
+}
+
+console.log('OK, we are starting...');
+prompt.start()
+
+prompt.get(schema, function(err, result) {
+  if (err) { return onErr(err); }
+  var entries = [];
+  var entry;
+  for (i=0;i<result.numberOfNumbers;i++) {
+    prompt.start();
+    entry = (i+1) + '). ' + random.createInteger(result.rangeMaximum);
+    entries.push(entry.yellow);
+  }
+  prompt.get(entries, function(){});
+})
